a744d2a5c538e0715d07a859ae1340e29e5ada52
enkuria <kengeene@gmail.com>
2024-06-28 23:22:50 +0300

    refactor: rewrite buttons component

--- src/app/_components/atoms/buttons.tsx
+++ src/app/_components/atoms/buttons.tsx
@@ -1,94 +1,80 @@
 import React from "react";
 
-export default function Buttons({buttonType, children, onClick, ...props}:{buttonType?: string, children: React.ReactNode, props?: any, onClick?: () => void}) {
-   switch (buttonType) {
-     case "primary":
-       return (
-         <PrimaryButton {...props} onClick={onClick}>
-           {children}
-         </PrimaryButton>
-       );
-
-     case "secondary":
-       return (
-         <SecondaryButton {...props} onClick={onClick}>
-           {children}
-         </SecondaryButton>
-       );
-
-     case "accent":
-       return (
-         <AccentButton {...props} onClick={onClick}>
-           {children}
-         </AccentButton>
-       );
-
-     default:
-       return (
-         <PrimaryButton {...props} onClick={onClick}>
-           {children}
-         </PrimaryButton>
-       );
-   }
-}
-
-function PrimaryButton({
-  children,
-  props,
-  onClick
-}: {
+type ButtonProps = {
+  buttonType?: "primary" | "secondary" | "accent";
   children: React.ReactNode;
-  props?: any;
-  onClick?: () => void
-}) {
-  return (
-    <button
-      onClick={onClick}
-      className="w-full rounded-md bg-transparent px-4 py-3 text-center font-semibold text-white transition duration-300 hover:bg-gray-700"
-      {...props}
-    >
-      {children}
-    </button>
-  );
-}
-
-
-function SecondaryButton({
-  children,
-  props,
-  onClick,
-}: {
-  children: React.ReactNode;
-  props?: any;
   onClick?: () => void;
-}) {
-  return (
-    <button
-      onClick={onClick}
-      className="w-full rounded-md bg-secondary px-4 py-3 text-center font-semibold text-white"
-      {...props}
-    >
-      {children}
-    </button>
-  );
-}
+};
 
-function AccentButton({
+const Button: React.FC<ButtonProps> = ({
+  buttonType = "primary",
   children,
-  props,
   onClick,
-}: {
-  children: React.ReactNode;
-  props?: any;
-  onClick?: () => void;
-}) {
-  return (
-    <button
-      onClick={onClick}
-      className="hover:bg-grey-600 w-full rounded-md bg-accent py-3 text-center font-semibold text-white transition duration-300"
-      {...props}
-    >
-      {children}
-    </button>
-  );
-}
+  ...props
+}) => {
+  switch (buttonType) {
+    case "secondary":
+      return (
+        <SecondaryButton onClick={onClick} {...props}>
+          {children}
+        </SecondaryButton>
+      );
+    case "accent":
+      return (
+        <AccentButton onClick={onClick} {...props}>
+          {children}
+        </AccentButton>
+      );
+    case "primary":
+    default:
+      return (
+        <PrimaryButton onClick={onClick} {...props}>
+          {children}
+        </PrimaryButton>
+      );
+  }
+};
+
+const PrimaryButton: React.FC<Omit<ButtonProps, "buttonType">> = ({
+  children,
+  onClick,
+  ...props
+}) => (
+  <button
+    onClick={onClick}
+    className="w-full rounded-md bg-transparent px-4 py-3 text-center font-semibold text-white transition duration-300 hover:bg-gray-700"
+    {...props}
+  >
+    {children}
+  </button>
+);
+
+const SecondaryButton: React.FC<Omit<ButtonProps, "buttonType">> = ({
+  children,
+  onClick,
+  ...props
+}) => (
+  <button
+    onClick={onClick}
+    className="w-full rounded-md bg-secondary px-4 py-3 text-center font-semibold text-white"
+    {...props}
+  >
+    {children}
+  </button>
+);
+
+const AccentButton: React.FC<Omit<ButtonProps, "buttonType">> = ({
+  children,
+  onClick,
+  ...props
+}) => (
+  <button
+    onClick={onClick}
+    className="hover:bg-grey-600 w-full rounded-md bg-accent py-3 text-center font-semibold text-white transition duration-300"
+    {...props}
+  >
+    {children}
+  </button>
+);
+
+export default Button;
