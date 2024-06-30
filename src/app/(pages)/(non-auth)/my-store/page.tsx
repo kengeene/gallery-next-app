e5c2e67e1812351aca495d382c3b4f3d351aa4d3
enkuria <kengeene@gmail.com>
2024-07-01 02:32:37 +0300

    refactor: rename AddPicture to AddPost

--- src/app/(pages)/(non-auth)/my-store/page.tsx
+++ src/app/(pages)/(non-auth)/my-store/page.tsx
@@ -1,17 +1,17 @@
 import ImageDisplay from "@/app/_components/organisims/gallery-display";
-import AddPicture from "@/app/_components/page/add-post";
+import AddPost from "@/app/_components/page/add-post";
 import { ComponentTitle } from "@/app/_components/molecules/component-title";
 
 export default async function Home() {
   return (
-      <div>
-        <div className="my-4 flex justify-between">
+    <div>
+      <div className="my-4 flex justify-between">
         <ComponentTitle title="My Store" subTitle="My recent uploads" />
-          <div>
-            <AddPicture/>
-          </div>
+        <div>
+          <AddPost />
         </div>
-          <ImageDisplay />
       </div>
+      <ImageDisplay />
+    </div>
   );
 }
--- src/app/(pages)/(non-auth)/page.tsx
+++ src/app/(pages)/(non-auth)/page.tsx
@@ -1,17 +1,17 @@
 import ImageDisplay from "@/app/_components/organisims/gallery-display";
-import AddPicture from "@/app/_components/page/add-post";
+import AddPost from "@/app/_components/page/add-post";
 import { ComponentTitle } from "@/app/_components/molecules/component-title";
 
 export default async function page() {
   return (
-      <div>
-        <div className="my-4 flex justify-between">
-          <ComponentTitle title="Gallery" subTitle="Community Gallery" />
-          <div>
-            <AddPicture/>
-          </div>
+    <div>
+      <div className="my-4 flex justify-between">
+        <ComponentTitle title="Gallery" subTitle="Community Gallery" />
+        <div>
+          <AddPost />
         </div>
-          <ImageDisplay />
       </div>
+      <ImageDisplay />
+    </div>
   );
 }
