e8a4c78f5fa8d9ed5e8a3697f231253f0dde7a49
enkuria <kengeene@gmail.com>
2024-07-01 03:04:50 +0300

    style: icon styling

--- src/app/_components/atoms/icons.tsx
+++ src/app/_components/atoms/icons.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function NameIconSmall({ name }: { name: string | null | undefined }) {
   return (
-    <div className="mr-1 flex h-8 w-8 items-center justify-center rounded-full bg-secondary py-2 text-sm font-bold text-light">
+    <div className="mr-1 flex h-6 w-6 items-center justify-center rounded-full bg-secondary py-2 text-sm font-bold text-light">
       {name?.charAt(0)}
     </div>
   );
@@ -10,7 +10,7 @@ export function NameIconSmall({ name }: { name: string | null | undefined }) {
 
 export function NameIconLarge({ name }: { name: string | null | undefined }) {
   return (
-    <div className="m-2 flex h-10 w-10 items-center justify-center rounded-full bg-secondary text-lg font-bold text-light">
+    <div className="m-2 flex w-11 h-11 items-center justify-center rounded-full bg-secondary text-lg font-bold text-light">
       {name?.charAt(0)}
     </div>
   );
