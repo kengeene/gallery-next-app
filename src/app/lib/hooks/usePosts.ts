3e08c2addb72504f6c4f820d5b4d03458c96d026
enkuria <kengeene@gmail.com>
2024-07-04 13:36:07 +0300

    refactor: invalidate posts after fetching

--- src/app/lib/hooks/usePosts.ts
+++ src/app/lib/hooks/usePosts.ts
@@ -1,22 +1,57 @@
-'use client'
+"use client";
 import { api } from "@/app/utils/api";
 
 const usePosts = () => {
- const { data, isLoading }= api.post.getAll.useQuery();
- const { data: myPosts, isLoading: isLoadingMyPosts } = api.post.get.useQuery();
-  const create = api.post.create.useMutation();
-  const like = api.post.like.useMutation();
-  const unlike = api.post.unlike.useMutation();
+  const utils = api.useUtils();
 
-   return {
-    posts: data ?? [], // Default to an empty array if posts is undefined
-    myPosts: myPosts ?? [], // Default to an empty array if posts is undefined
+  const { data, isLoading } = api.post.getAll.useQuery(undefined, {
+    refetchOnWindowFocus: true,
+    refetchOnReconnect: true,
+  });
+  const { data: myPosts, isLoading: isLoadingMyPosts } = api.post.get.useQuery(
+    undefined,
+    {
+      refetchOnWindowFocus: true,
+      refetchOnReconnect: true,
+    },
+  );
+
+  const create = api.post.create.useMutation({
+    onSuccess: async() => {
+      // Invalidate all queries in the post router
+      await utils.post.invalidate();
+    },
+  });
+
+  const like = api.post.like.useMutation({
+    onSuccess: async() => {
+      // Invalidate the specific post that was liked
+      await utils.post.getAll.invalidate();
+      await utils.post.get.invalidate();
+      // If you have a getById query, you could invalidate it specifically:
+      // utils.post.getById.invalidate({ id: variables.postId });
+    },
+  });
+
+  const unlike = api.post.unlike.useMutation({
+    onSuccess: async() => {
+      // Invalidate the specific post that was unliked
+      await utils.post.getAll.invalidate();
+      await utils.post.get.invalidate();
+      // If you have a getById query, you could invalidate it specifically:
+      // utils.post.getById.invalidate({ id: variables.postId });
+    },
+  });
+
+  return {
+    posts: data ?? [],
+    myPosts: myPosts ?? [],
     isLoading,
+    isLoadingMyPosts,
     create,
     like,
-    unlike
+    unlike,
   };
-}
-
+};
 
 export default usePosts;
