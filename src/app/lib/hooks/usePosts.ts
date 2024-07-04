196748dc23205d79d39d754fc258c721ef147288
enkuria <kengeene@gmail.com>
2024-07-04 18:46:22 +0300

    perf: remove unecessary fetches by using onSettled event and refetch function

--- src/app/lib/hooks/usePosts.ts
+++ src/app/lib/hooks/usePosts.ts
@@ -1,45 +1,38 @@
-"use client";
 import { api } from "@/app/utils/api";
 
 const usePosts = () => {
-  const utils = api.useUtils();
-
-  const { data, isLoading } = api.post.getAll.useQuery(undefined, {
-    refetchOnWindowFocus: true,
-    refetchOnReconnect: true,
+  const { data, isLoading, refetch } = api.post.getAll.useQuery(undefined, {
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
   });
   const { data: myPosts, isLoading: isLoadingMyPosts } = api.post.get.useQuery(
     undefined,
     {
-    refetchOnWindowFocus: true,
-    refetchOnReconnect: true,
-  },
-);
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+    },
+  );
 
   const create = api.post.create.useMutation({
-    onSuccess: async() => {
-      // Invalidate all queries in the post router
-      await utils.post.invalidate();
+    onSettled: async() => {
+      // refetch posts after create
+      await refetch();
     },
   });
 
   const like = api.post.like.useMutation({
-    onSuccess: async() => {
-      // Invalidate the specific post that was liked
-      await utils.post.getAll.invalidate();
-      await utils.post.get.invalidate();
-      // If you have a getById query, you could invalidate it specifically:
-      // utils.post.getById.invalidate({ id: variables.postId });
+    onSettled: async() => {
+      // refetch posts after like
+      await refetch();
     },
   });
 
   const unlike = api.post.unlike.useMutation({
-    onSuccess: async() => {
-      // Invalidate the specific post that was unliked
-      await utils.post.getAll.invalidate();
-      await utils.post.get.invalidate();
-      // If you have a getById query, you could invalidate it specifically:
-      // utils.post.getById.invalidate({ id: variables.postId });
+    onSettled: async () => {
+      // refetch posts after unlike
+      await refetch();
     },
   });
 
